59d0e148158428ee317bdb87d3feecbb1f0d2b20
cyrusbuilt <cyrusbuilt@gmail.com>
2025-01-17 14:27:45 -0500

    Adding IOEX driver

---
+++ Kernel/Devices/ioex.c
@@ -0,0 +1,38 @@
+#include <Kernel/opcodes.h>
+#include <Kernel/Devices/ioex.h>
+#include <Common/io.h>
+
+void ioex_write(enum ioex_port_t port, uint8_t data) {
+    if (port == ioex_port_a) {
+        STORE_OPC_PORT = OP_IO_WR_GPIOA;
+    } else {
+        STORE_OPC_PORT = OP_IO_WR_GPIOB;
+    }
+
+    EXEC_PORT = (char)(data);
+}
+
+void ioex_set_mode(enum ioex_port_t port, enum ioex_mode_t mode) {
+    if (port == ioex_port_a) {
+        STORE_OPC_PORT = OP_IO_WR_IODIRA;
+    } else {
+        STORE_OPC_PORT = OP_IO_WR_IODIRB;
+    }
+
+    char mode_val = 0;
+    if (mode == ioex_mode_output)  {
+        mode_val = 1;
+    }
+
+    EXEC_PORT = mode_val;
+}
+
+void ioex_set_pullup(enum ioex_port_t port, bool pullup) {
+    if (port == ioex_port_a) {
+        STORE_OPC_PORT = OP_IO_WR_GPPUA;
+    } else {
+        STORE_OPC_PORT = OP_IO_WR_GPPUB;
+    }
+
+    EXEC_PORT = (char)(pullup);
+}
---
+++ Kernel/Devices/ioex.h
@@ -0,0 +1,25 @@
+/**
+* IOEX: I/O Expansion driver. Provides drivers functions for interfacing
+* with the IOEX functions exposed in firmware.
+*/
+#ifndef _IOEX_H_
+#define _IOEX_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+enum ioex_port_t {
+    ioex_port_a,
+    ioex_port_b
+};
+
+enum ioex_mode_t {
+    ioex_mode_input,
+    ioex_mode_output
+};
+
+void ioex_write(enum ioex_port_t port, uint8_t data);
+void ioex_set_mode(enum ioex_port_t port, enum ioex_mode_t mode);
+void ioex_set_pullup(enum ioex_port_t port, bool pullup);
+
+#endif
